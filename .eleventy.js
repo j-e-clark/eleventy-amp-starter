1056d8d702ba7ccc396b34b09799a718a599bdd3
James <james@jeclark.co.uk>
2020-10-27 18:04:22 +0000

    Added missing Eleventy config

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const ampPlugin = require('@ampproject/eleventy-plugin-amp');
+const pluginSass = require('eleventy-plugin-sass');
+const dir = {
+  input: './src/site',
+};
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({ 'src/static': '/' });
+  eleventyConfig.addPassthroughCopy({ 'src/img': 'img' });
+  eleventyConfig.addPlugin(ampPlugin, {
+    imageBasePath: 'src',
+    imageOptimization: true,
+  });
+  eleventyConfig.addPlugin(pluginSass, {
+    outputDir: 'src/site/_includes/css',
+    remap: true,
+  });
+
+  return {
+    dir,
+  };
+};
